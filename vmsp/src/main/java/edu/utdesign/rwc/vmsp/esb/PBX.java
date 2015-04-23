cc22cd884160780053c4dc4196ddd6b9316a4939
Omar Darwish <om.drwsh@gmail.com>
2015-04-22 21:33:55 -0500

    Fixed tab spacing

--- vmsp/src/main/java/edu/utdesign/rwc/vmsp/esb/PBX.java
+++ vmsp/src/main/java/edu/utdesign/rwc/vmsp/esb/PBX.java
@@ -12,55 +12,54 @@ import org.exolab.castor.xml.Unmarshaller;
 import org.xml.sax.InputSource;
 
 public class PBX {
-	private long lastUpdated;
-	private boolean isOn;
-	private HashSet<Phone> phones;
-	private HashSet<Radio> radios;
+   private long lastUpdated;
+   private boolean isOn;
+   private HashSet<Phone> phones;
+   private HashSet<Radio> radios;
 
-	public PBX() {
-		this.lastUpdated = System.currentTimeMillis() / 1000L;
-		this.isOn = false;
-		this.phones = new HashSet<Phone>();
-		this.radios = new HashSet<Radio>();
-	}
+   public PBX() {
+      this.lastUpdated = System.currentTimeMillis() / 1000L;
+      this.isOn = false;
+      this.phones = new HashSet<Phone>();
+      this.radios = new HashSet<Radio>();
+   }
 
-	public PBX(boolean isOn, HashSet<Phone> phones, HashSet<Radio> radios) {
-		super();
-		this.isOn = isOn;
-		this.phones = phones;
-		this.radios = radios;
-	}
+   public PBX(boolean isOn, HashSet<Phone> phones, HashSet<Radio> radios) {
+      this.isOn = isOn;
+      this.phones = phones;
+      this.radios = radios;
+   }
 
-	public long getLastUpdated() {
-		return lastUpdated;
-	}
+   public long getLastUpdated() {
+      return lastUpdated;
+   }
 
-	public void setLastUpdated(long lastUpdated) {
-		this.lastUpdated = lastUpdated;
-	}
+   public void setLastUpdated(long lastUpdated) {
+      this.lastUpdated = lastUpdated;
+   }
 
-	public boolean isOn() {
-		return isOn;
-	}
+   public boolean isOn() {
+      return isOn;
+   }
 
-	public void setOn(boolean isOn) {
-		this.isOn = isOn;
-	}
+   public void setOn(boolean isOn) {
+      this.isOn = isOn;
+   }
 
-	public HashSet<Phone> getPhones() {
-		return phones;
-	}
+   public HashSet<Phone> getPhones() {
+      return phones;
+   }
 
-	public void setPhones(HashSet<Phone> phones) {
-		this.phones = phones;
-	}
+   public void setPhones(HashSet<Phone> phones) {
+      this.phones = phones;
+   }
 
-	public HashSet<Radio> getRadios() {
-		return radios;
-	}
+   public HashSet<Radio> getRadios() {
+      return radios;
+   }
 
-	public void setRadios(HashSet<Radio> radios) {
-		this.radios = radios;
-	}
+   public void setRadios(HashSet<Radio> radios) {
+      this.radios = radios;
+   }
 
 }
